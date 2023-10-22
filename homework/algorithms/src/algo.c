66905d87b812a63209b75e8e82cff38339160080
JCDM122300 <55261863+JCDM122300@users.noreply.github.com>
2023-10-21 21:11:57 -0500

    Attempted To Finish HW
    Still need to figure out this makefile stuff otherwise I'll just give the link as is.

--- homework/algorithms/src/algo.c
+++ homework/algorithms/src/algo.c
@@ -1,21 +1,74 @@
 #include "algo.h"
+#include <stdio.h>
+#include <math.h>
 
 long fibonacci(const int n)
 {
-    return 0;
+    if (n == 1) {
+        return 0;
+    }
+    else if (n == 2) {
+        return 1;
+    }
+    else if (n == 3) {
+        return 1;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2); 
+    }
+
+    
+ 
 }
 
 int reverse(const int n)
 {
+    reversenum = 0, remainder;
+    printf("Enter the integer you want reversed: ");
+    scanf("&d", &n);
+    while (n != 0) {
+        remainder = n % 10;
+        reversenum = reversenum * 10 + remainder;
+        n /= 10;
+    }
+    printf("The reversed number is: = %d", reversenum);
     return 0;
 }
 
-int prime_factor(const int n)
+long long prime_factor(long long n)
 {
-    return 0;
+    long long LPrime = -1;
+
+    if (n % 2 == 0) {
+        LPrime = 2;
+        while (n % == 0) {
+            n = n / 2;
+        }
+    }
+    for (long long i = 3; i <= sqrt(n); i += 2) {
+        while (n % i == 0) {
+            LPrime = i;
+            n = n / i;
+        }
+    }
+    if (n > 2) {
+        LPrime = n;
+    }
+    return LPrime;
+
+    
 }
 
 int prime_sum(const int n)
 {
-    return 0;
+    int i, j;
+    for (i = 1; i <= 1000; i++) {
+        if (i % j == 0) {
+            break;
+        }
+
+    }
+    if (i == j) {
+        sum = i + j;
+    }
 }
